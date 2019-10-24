ed57d122680b5df458dec17809c53b67a5b3bf9b
JesTestSie <Jessie.Eurenius@yh.nackademin.se>
2019-10-24 22:30:37 +0200

    Tog bort överdrivna kommentarer från master.

--- src/GameHandlers.java
+++ src/GameHandlers.java
@@ -4,23 +4,17 @@ import java.util.List;
 
 public class GameHandlers {
 
-    public List<JButton> randomGame(List<JButton> buttons) {
+    public void randomGame(List<JButton> buttons) {
         Collections.shuffle(buttons);
-        return buttons;
     }
 
     public boolean checkWinner(List<JButton> buttons, List<String> winList) {
-        // Denna metod kallas efter varje button tryck (ActionListener)
 
-        // Tar både knapp listan och vinst listan
-        // Loopar 16 gånger.
         for (int i = 0; i < 16; i++) {
-            // Sätter number till texten i alla knappar
             String number = buttons.get(i).getText();
             System.out.println("buttons nr is = " + number);
             System.out.println("winList nr is = " + winList.get(i));
-            // Om numret inte stämmer med den i vinstlistan så retunerar den false
-            // Skulle alla nummer stämma så går vi ur if-loopen och retunerar true.
+
             if (!number.equals(winList.get(i))) {
                 return false;
             }
@@ -31,19 +25,7 @@ public class GameHandlers {
     }
 
     public boolean solvable(List<JButton> buttons) {
-        // Lättare att förklara på plats
 
-        /*
-        * Kort förklarat
-        * Jag räknar ut vilken rad den blanka rutan ligger på
-        * Är den på en "jämn" eller "ojämn" plats (jämn = 2 - 4 - 6) i vårat fall är det bara rad 2 och 4 som är jämn
-        * och 1-3 ojämn.
-        * Om raden är jämn så får inte Inversion vara jämn
-        * Om raden är ojämn så får inte Inversion vara ojämn
-        * Inversion är när numret på plats 1 är större än numret på plats 2.
-        * T.e.x. Om spelplanen såg ut såhär (2, 5, 6, 1, 8, 10, 3) så är det 2 Inversions
-        * 6 är större än 1 och 10 är större än 3.
-        *  */
         int antal = 0;
         int row = -1;
         for (int i = 0; i < buttons.size() - 1; i++) {
